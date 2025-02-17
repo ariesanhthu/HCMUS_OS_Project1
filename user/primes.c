130080990a8636195cfdb9c7e5460c070b2e73c8
Nguyễn Anh Thư <75130314+ariesanhthu@users.noreply.github.com>
2025-02-17 21:35:45 +0700

    Update primes.c

--- user/primes.c
+++ user/primes.c
@@ -2,7 +2,8 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-void primes(int p_read) __attribute__((noreturn)); //prevent infinite recursive call error
+//prevent infinite recursive call error
+void primes(int p_read) __attribute__((noreturn)); 
 
 void primes(int p_read) 
 {
@@ -22,27 +23,32 @@ void primes(int p_read)
 
     int pid = fork();
     
-    if (pid == 0) //child
+    //child
+    if (pid == 0) 
     {
         close(p[1]);
         close(p_read);
 
-        primes(p[0]); //recursively call primes w new p_read
+        //recursively call primes w new p_read
+        primes(p[0]); 
     } 
-    else if (pid > 0) //parent
+    else 
+    //parent
+    if (pid > 0) 
     {
-        close(p[0]); //close p_read in the parent process
+        //close p_read in the parent process
+        close(p[0]); 
 
         while (read(p_read, &num, sizeof(num)) != 0) 
-        {
             if (num % prime != 0) //sieve 
-            {
                 write(p[1], &num, sizeof(num));
-            }
-        }
 
-        close(p[1]); //this pipe
-        close(p_read); // previous pipr
+        //current pipe
+        close(p[1]); 
+      
+        // previous pipe
+        close(p_read); 
+      
         wait(0);
         exit(0);
     } 
@@ -60,24 +66,31 @@ int main(int argc, char *argv[])
     pipe(p);
 
     int pid = fork();
-    if (pid == 0) //child
+  
+    //child
+    if (pid == 0) 
     {
         close(p[1]);
         primes(p[0]);//start w p_read of init pipe
     } 
-    else if (pid > 0) //parent
+    else 
+    //parent
+    if (pid > 0) 
     {
         close(p[0]);
 
         //feed num from 2-->280
-        for (int i = 2; i <= 280; i++) {
+        for (int i = 2; i <= 280; i++) 
             write(p[1], &i, sizeof(i));
-        }
 
         close(p[1]);
-        wait(0); // wait until the entire pipeline terminates, 
+        // wait until the entire pipeline terminates, 
+        wait(0); 
+        
         exit(0);
-    } else {
+    } 
+    else 
+    {
         printf("\nFork error\n");
         exit(1);
     }
